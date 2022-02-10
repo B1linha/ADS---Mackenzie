6d19b8972130ad7f431f1e871cc37996074966d2
lucasbarreto <lucasbrittobr@gmail.com>
2022-02-09 22:24:36 -0300

    Primeiras atividades
    Atividades em HTML/CSS e Python

---
+++ HTML/CSS & PYTHON/Python/Ex002.py
@@ -0,0 +1,6 @@
+"""2. Faça um programa que receba três notas, calcule e mostre a média aritmética entre elas."""
+n1 = float(input('Digite uma nota'))
+n2 = float(input('Digite outra nota'))
+n3 = float(input('Digite outra nota'))
+media = (n1+n2+n3)/3
+print ('Media das notas = %.1f  ' %media)
---
+++ HTML/CSS & PYTHON/Python/Ex004.py
@@ -0,0 +1,4 @@
+""" Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabende-se que este sofreu um aumento de 25%""" 
+sal = float(input('Salário:'))
+nsal = sal*1.25
+print ('novo salário = ', nsal)
