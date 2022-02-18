fef1d78e234cff7a315bd4ddf8d7c3003f46abc0
lucasbarreto <lucasbrittobr@gmail.com>
2022-02-18 15:12:06 -0300

    activity 2
    Inicio da atividade 2 de Algoritmos e Programação.

---
+++ HTML/PYTHON/Atividade 2/index.py
@@ -0,0 +1,8 @@
+"""Faça um programa em Python que receba o custo (valor em reais) de um espetáculo teatral e o preço do convite (valor em reais) desse espetáculo. Esse programa deve calcular e mostrar:
+a) A quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado.  
+
+b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23% """ 
+Custo = int(input('Digite o custo do espetáculo'))
+PreçoConvite = int(input('Digte o preço do convite'))
+ConvitesM = Custo/PreçoConvite
+
