66ed7d49956ffa5f0fbe9eedb810440d4499b636
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-02-20 11:16:04 -0300

    Python 1.0
    Atividades de python

--- Mackenzie-ADS/PYTHON/Atividade 2/Ex001.py
+++ Mackenzie-ADS/PYTHON/Atividade 2/Ex001.py
@@ -1,10 +1,13 @@
 """Faça um programa em Python que receba o custo (valor em reais) de um espetáculo teatral e o preço do convite (valor em reais) desse espetáculo. Esse programa deve calcular e mostrar:
 a) A quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado.  
-
+25000.00 / 32.00 = 782 / 961
 b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23% """ 
-Custo = int(input('Digite o custo do espetáculo'))
-PreçoConvite = int(input('Digte o preço do convite'))
-ConvitesM = Custo/PreçoConvite
-Lucro = Custo*23/100/PreçoConvite
-print('A quantidade de convites mínimos é:', ConvitesM)
-print ('Para ter um lucro de 23% devem ser vendidos:', Lucro)
+import math
+Custo = float(input(''))
+PreçoConvite = float(input(''))
+X=Custo/PreçoConvite
+XS = math.ceil(X)
+Y = X + X*0.23
+YS = math.ceil(Y)
+print(XS)
+print(YS)
