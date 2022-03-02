b03e53befad19c7cc95d51f93422fad72fb9a50c
lucasbarreto <lucasbrittobr@gmail.com>
2022-03-02 10:05:37 -0300

    Python 1.4

--- PYTHON/Aula 4/Ex001.py
+++ PYTHON/Aula 4/Ex001.py
@@ -2,3 +2,13 @@
 média aritmética e a mensagem constante na tabela a seguir. Aos alunos que
 ficaram para exame, calcule e mostre a nota que deverão tirar para serem
 aprovados, considerando que a média exigida é 6,0."""
+n1 = float(input())
+n2 = float(input())
+n3 = float (input())
+media = (n1+n2+n3)/3
+if media >= 0 and media < 3:
+    print('Sua média é {:.1f} você está Reprovado!'.format(media))
+elif media < 7:
+    print('Sua média é {:.1f} você tem que fazer um Exame!'.format(media))
+else: 
+    print('Sua média é {:.1f} você está Aprovado!'.format(media))
---
+++ PYTHON/Aula 4/Ex002.py
@@ -0,0 +1,4 @@
+"""Elabore um programa que calcule e mostre o valor que deve ser pago por um produto, considerando
+que o usuário fornecerá o preço normal de etiqueta e o código da condição de pagamento.
+Utilize os códigos da tabela seguinte para ler qual é a condição de pagamento escolhida e efetuar o
+cálculo adequado."""
