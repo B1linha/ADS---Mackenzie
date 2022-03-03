44776928c4c3e9a1e3b96e5c4796d188332564b5
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-03 11:41:54 -0300

    JavaScript 1.3

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,20 @@
+const main = (params) => {
+       //code here!!
+} 
+ exibirNumerosPrimos(1000);
+
+ function exibirNumerosPrimos(limite){
+   for(let numero =2; numero <= limite; numero++) {
+     
+     if (NumeroPrimo(numero)) console.log(numero);
+   }
+ }
+ function NumeroPrimo(numero) {
+ 
+   for (let divisor = 2; divisor < numero; divisor++) {
+     if(numero % divisor === 0) {
+       return false;
+     }
+   }
+   return true;
+ }
