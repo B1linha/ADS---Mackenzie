90d35329515fa0b02e49f168ea68688c7359a1b1
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-07 14:59:43 -0300

    JavaScript 1.9

--- JAVASCRIPT/Site Contador/script.js
+++ JAVASCRIPT/Site Contador/script.js
@@ -1,12 +1,23 @@
-function contar() {
-    var inicio = window.document.getElementById('txti')
-    var fim = window.document.getElementById('txtf')
-    var passo =  window.document.getElementById('txtp')
-    var res = window.document.getElementById('res')
-    while (res <= fim){
-        inicio = inicio + passo
-        res.style.textAlign = 'center'
-        res = inicio
-        console.log(res)
+// IIFE  Immediately-invoked Function Expression
+function meuEscopo() {
+    const form = document.querySelector('.form'); // Se fosse ID usaria o #
+    const res = document.querySelector('.res');
+    const pessoas = []; // Criando array onde os dados vão ficar guardados
+    function recebeEventoForm (evento) {
+        evento.preventDefault(); //Evita que a página recarregue ao enviar form
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        pessoas.push({ // Adicionando valores na array pessoas com o push
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        console.log(pessoas);
+        res.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`
     }
-} 
+    form.addEventListener('submit', recebeEventoForm); // Escuta o evento submit
+}
+meuEscopo(); 
