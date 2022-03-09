6dceb816c1bc9554952c41ba638662cddec1e3c4
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-09 15:50:34 -0300

    JavaScript 2.2

--- JAVASCRIPT/Calcule seu IMC/assets/js/main.js
+++ JAVASCRIPT/Calcule seu IMC/assets/js/main.js
@@ -4,17 +4,16 @@ const form = document.querySelector('#form');
 form.addEventListener('submit', function (event) { // Evento para evitar a atualização da page
     event.preventDefault(); 
     console.log('Evento previnido');
-    result('Olá mundo!')
+    result('Olá mundo!') 
 });
 
 function criaP () {
     const p = document.createElement('p');// Criando um parágrafo 
-    p.classList.add('p-res') // Adiciona uma class
-    p.innerHTML = 'Qualquer coisa';
-    resultado.appendChild(p);  // Insere um elemento (filho) na div res
+    return p;
 }
 
 function result (msg) { // Tudo que eu colocar no parâmetro msg vai aparecer na div res
     const resultado = document.querySelector('#res');
     resultado.innerHTML = ''; 
+    const p = criaP();
 }   
