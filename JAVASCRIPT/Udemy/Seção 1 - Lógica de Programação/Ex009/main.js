4749566ca864ca2bb7e34dc892831026ff7f1594
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-18 20:51:06 -0300

    JavaScript 3.8

---
+++ JAVASCRIPT/Udemy/Seção 1 - Lógica de Programação/Ex009/main.js
@@ -0,0 +1,41 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus(); // focus() Permite continuar digitando em um campo sem clicar nele
+}
+
+function botaoApagar(li) {
+    li.innerHTML += ' '; // Da um espaço entre o texto e o botão
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = 'Apagar';
+    li.appendChild(botaoApagar);
+} 
+
+function criaTarefa(texto) {
+    const li = criaLi();
+    tarefas.appendChild(li);
+    li.innerHTML = texto;
+    limpaInput();
+    botaoApagar(li)
+}
+
+inputTarefa.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) { // 13 é o code do ENTER
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value)
+        
+}});
+
+
+btnTarefa.addEventListener('click', function() {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value)
+});
