f40a8b2ee568e823e868da0b32c4efe7056d47b7
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-19 16:56:25 -0300

    JavaScript 4.0

--- JAVASCRIPT/Guanabara/Vetores-Array.js
+++
@@ -1,6 +0,0 @@
-let num = [5, 8, 4,]
-num[3] = 6 // acrescenta 1 número na casa destinada
-num.push(7) // acrescenta 1 número na última casa
-num.length // tamanha do índice
-num.sort() // embaralha os números
-console.log(`Nosso vetor tem ${num.length} posições`)
