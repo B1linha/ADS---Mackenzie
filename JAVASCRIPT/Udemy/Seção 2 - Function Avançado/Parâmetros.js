c94aeff10cbffc8243f2e3c1d027096ec36c475a
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-21 19:50:52 -0300

    JavaScript 4.2

---
+++ JAVASCRIPT/Udemy/Seção 2 - Function Avançado/Parâmetros.js
@@ -0,0 +1,15 @@
+// Arguments sustenta todos os argumentos enviados nos parâmetros
+function funcao() {
+    console.log(arguments[0])
+}
+funcao('Valor', 1, 2, 3, 4, 5);
+
+function soma () {
+    let total = 0;
+    for (let argumento of arguments) {
+        total += argumento; 
+    }
+    console.log(total); 
+}
+soma(2, 3, 4, 5, 6, 7, 9)
+
