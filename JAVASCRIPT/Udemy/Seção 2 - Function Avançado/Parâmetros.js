c4f74b092570b3914d68fbcf0b2593ad7f6deaea
lucasbarreto <99029800+B1linha@users.noreply.github.com>
2022-03-24 14:47:50 -0300

    JavaScript 4.4

--- JAVASCRIPT/Udemy/Seção 2 - Function Avançado/Parâmetros.js
+++ JAVASCRIPT/Udemy/Seção 2 - Function Avançado/Parâmetros.js
@@ -4,6 +4,7 @@ function funcao() {
 }
 funcao('Valor', 1, 2, 3, 4, 5);
 
+// Arguments não existe em arrow function    
 function soma () {
     let total = 0;
     for (let argumento of arguments) {
@@ -15,3 +16,23 @@ soma(2, 3, 4, 5, 6, 7, 9)
 
 // Atribuição via desestruturação em parâmetros
 
+function obj ({ nome, sobrenome, idade }) {
+    console.log(nome, sobrenome, idade);
+}
+obj({ nome: 'Lucas', sobrenome: 'Barreto', idade: 19 })
+
+function arrays ([valor1, valor2, valor3]) {
+    console.log(valor1, valor2, valor3);
+}
+arrays(['Lucas', 'Barreto', 19])
+
+const conta = (operador, acumulador, ...numeros) => {
+    for (let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '*') acumulador *= numero;
+        if (operador === '/') acumulador /= numero;
+    }
+    console.log(acumulador);
+};
+conta('+', 1, 30, 40, 50, 60);
